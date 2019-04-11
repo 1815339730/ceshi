fc20bdb4da7e71cbc25971cf1cea7d95d34d99ff
suruihuai <1815339730@qq.com>
2019-04-11 19:18:04 +0800

    测试

---
+++ dangong/app/src/main/java/com/example/dangong2/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dangong2.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      /*  BorderView border = new BorderView(this);
+
+        border.setBackgroundResource(R.drawable.border_red);
+        //border.setBackgroundColor(Color.GREEN);
+
+        //border.getEffect().setScale(1.1f);
+        ViewGroup list = (ViewGroup) findViewById(R.id.list);
+        border.attachTo(list);*/
+
+    }
+}
